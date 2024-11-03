8484310c47ff5797a5c30322d4c6b517157a6ba1
Maxim Zakharov <maxeozakh@gmail.com>
2024-11-03 20:19:57 +0100

    change args for llamacli to be ready for sockets

--- app/llama.py
+++ app/llama.py
@@ -2,7 +2,7 @@ import subprocess
 
 PATH_TO_LLAMA = './llama.cpp/'
 MODEL_NAME = 'gemma-1.1-7b-it.Q4_K_M.gguf'
-SUMMARY_LENGTH = 26
+SUMMARY_LENGTH = 500
 
 
 def run_llama(prompt, summa_len=SUMMARY_LENGTH):
@@ -10,15 +10,19 @@ def run_llama(prompt, summa_len=SUMMARY_LENGTH):
         f'{PATH_TO_LLAMA}llama-cli',
         '-m', f'{PATH_TO_LLAMA}models/{MODEL_NAME}',
         '--chat-template', 'gemma',
-        '--no-display-prompt',
-        '-r', "User:",
-        '--in-prefix', "",
-        '--in-suffix', ">>>>>>>>",
         '--no-warmup',
+        '--no-display-prompt',
+        '-i',
         '--prompt', f'Summarize the following text in {
             summa_len} words: {prompt}'
+        # '-r', "User:",
+        # '--in-prefix', "",
+        # '--in-suffix', ">>>>>>>>",
     ]
 
-    result = subprocess.run(
-        command, stdout=subprocess.PIPE, text=True, check=True)
-    return result.stdout
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, universal_newlines=True)
+
+    for line in process.stdout:
+        words = line.strip()
+        print('NEW LINE', words)
